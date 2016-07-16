0977c5b8ac82a00a5909d41f24f7d0cc2919a502
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-16 13:54:26 -0500

    Detached base template

--- templates/base/media/left_sidebar_addon.js
+++
@@ -1,9 +0,0 @@
-
-function toggle_left_sidebar_items()
-{
-    var visible        = $('#left_sidebar').is(':visible');
-    var bar_visibility = visible ? 'false' : 'true';
-    
-    $('body').attr('data-left-sidebar-visible', bar_visibility);
-    $('#left_sidebar_trigger').toggle();
-}
