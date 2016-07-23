c5ef2cfa4a4e2196557cfd71bb3131736753027b
Alejandro Caballero <lava.caballero@gmail.com>
2016-07-23 00:46:13 -0500

    Added documents renderer

---
+++ scripts/document_handler.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Document handler - Includes all registered document handlers and lets them stop on hit.
+ *
+ * @package    HNG2
+ * @subpackage Core::public_html
+ * @author     Alejandro Caballero - lava.caballero@gmail.com
+ * 
+ * $_GET params:
+ * @param handle
+ */
+
+use hng2_base\module;
+
+include "../config.php";
+include "../includes/bootstrap.inc";
+
+/** @var module[] $modules */
+foreach($modules as $module)
+{
+    if( ! isset($module->php_includes) ) continue;
+    if( ! isset($module->php_includes->document_handler) ) continue;
+    
+    $include = ABSPATH . "/{$module->name}/{$module->php_includes->document_handler}";
+    if( ! is_file($include) ) continue;
+    
+    $current_module = $this_module = $module;
+    include $include;
+}
+
+throw_fake_404();
