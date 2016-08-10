38b8b13b1718533a8b98d30964ed23121e69345f
Alejandro Caballero <lava.caballero@gmail.com>
2016-08-09 20:49:36 -0500

    Tuned processing functions to avoid nested anchors

--- classes/hng2_media/media_record.php
+++ classes/hng2_media/media_record.php
@@ -151,14 +151,18 @@ class media_record extends abstract_record
     
     /**
      * Returns the title with all output processing.
+     *
+     * @param bool $include_autolinks If false, <a> tags wont be added. Useful when the title is inserted into an <a> tag.
+     *
+     * @return string
      */
-    public function get_processed_title()
+    public function get_processed_title($include_autolinks = true)
     {
         global $config;
         
         $contents = $this->title;
         $contents = convert_emojis($contents);
-        $contents = autolink_hash_tags($contents, "{$config->full_root_path}/tag/", "/media");
+        if( $include_autolinks ) $contents = autolink_hash_tags($contents, "{$config->full_root_path}/tag/", "/media");
         
         # TODO: Add get_processed_title() extension point
         
