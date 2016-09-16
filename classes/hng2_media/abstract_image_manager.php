e8c3f9f6d5b610d48b8b52d114af026aa1ec1f32
Alejandro Caballero <lava.caballero@gmail.com>
2016-09-16 02:35:21 -0500

    Added missing dimensions parameter

--- classes/hng2_media/abstract_image_manager.php
+++ classes/hng2_media/abstract_image_manager.php
@@ -4,4 +4,14 @@ namespace hng2_media;
 abstract class abstract_image_manager extends abstract_item_manager
 {
     protected $media_type = "image";
+    
+    public function get_dimensions()
+    {
+        if( ! empty($this->dimensions) ) return $this->dimensions;
+        
+        $parts = @getimagesize($this->file_path);
+        if( $parts ) $this->dimensions = "{$parts[0]}x{$parts[1]}";
+        
+        return $this->dimensions;
+    }
 }
