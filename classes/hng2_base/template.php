1aed0ff6c43ab08000fef9480eac78fbecff9bc5
Alejandro Caballero <lava.caballero@gmail.com>
2017-05-09 14:19:56 -0500

    Refactored data key

--- classes/hng2_base/template.php
+++ classes/hng2_base/template.php
@@ -453,7 +453,7 @@ class template
         global $modules;
         
         /** @noinspection PhpUnusedLocalVariableInspection */
-        $data_key = "_right_sidebar_widgets:{$this_module->name}.{$widget["id"]}" . (empty($seed) ? "" : "_{$seed}");
+        $data_key = "{{$widget["for"]}}-{$this_module->name}.{$widget["id"]}" . (empty($seed) ? "" : "-{$seed}");
         
         if( $widget["type"] == "php" )
         {
