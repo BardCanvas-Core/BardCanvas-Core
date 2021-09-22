dfcd5b5475672ccca00a8ea02037bb46c3525277
Alejandro Caballero <lava.caballero@gmail.com>
2021-09-22 09:11:00 -0500

    Relocated method

--- classes/hng2_base/account.php
+++ classes/hng2_base/account.php
@@ -401,6 +401,23 @@ class account extends account_toolbox
         return $res;
     }
     
+    public function add_to_changelog($caption, $details = "")
+    {
+        global $database;
+        
+        $date = date("Y-m-d H:i:s");
+        $text = "• [$date] - $caption";
+        if( empty($details) ) $text .= "\n\n";
+        else                  $text .= ":\n$details\n\n";
+        
+        $text = addslashes($text);
+        
+        $database->exec("
+            update account set changelog = concat(changelog, '$text')
+            where id_account = '$this->id_account'
+        ");
+    }
+    
     /**
      * Sends a confirmation email to new account creator with a token
      */
